d60350dadeb76b02b0bee0327b6c85de9b2c111e
dmitry <kalibdune@yandex.ru>
2025-04-01 21:10:34 +0300

    add mvp Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import { useState } from 'react'
 import './App.css'
+import Main from './pages/Main/Main'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/types/Card.tsx
@@ -0,0 +1,8 @@
+export interface CardType {
+    title: string
+    description: string
+    price: number
+    id: number
+    x: number
+    y: number
+}
